f87b69320d985a34238f40edb6cf0d7a6cb22df3
yousefse3a <62356610+yousefse3a@users.noreply.github.com>
2023-06-16 20:06:16 +0300

    Update DBconnection.js

--- connections/DBconnection.js
+++ connections/DBconnection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = () => {
-    return mongoose.connect(process.env.DBURL)
+    // return mongoose.connect(process.env.DBURL)
+    return mongoose.connect('mongodb+srv://se3a:ZkRg57L6ZYTOuCQU@cluster0.sxlie.mongodb.net/ecommerce?retryWrites=true&w=majority')
         .then((result) => { console.log('connect DB') })
         .catch((err) => { console.log({ 'fail DB': err }) })
 }
